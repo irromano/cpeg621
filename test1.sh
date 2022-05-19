bebe71f380d8ec15d2f6a911a9ff0e2106502bc4
irromano <iromano@udel.edu>
2022-05-18 20:54:50 -0400

    Editing script echo calls

--- test1.sh
+++ test1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo No CSE Program Test:
 ~/cpeg621/Lab3_bb3exp/bb3exp ~/cpeg621/test1.txt
+echo
+echo Task 1 CSE Program Test:
 ~/cpeg621/FinalProject/cse3exp ~/cpeg621/test1.txt
+echo
+echo Task 2 Heuristic CSE Program Test:
 ~/cpeg621/FinalProject_2/cse3exp2 ~/cpeg621/test1.txt
